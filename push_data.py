f92739091391d4641e7e4ee95fcb2eef68265349
anshu052005 <p20anshu05@gmail.com>
2026-07-13 00:08:30 +0530

    ETL Pipeline

--- push_data.py
+++ push_data.py
@@ -50,13 +50,21 @@ class NetworkDataExtract():
         
 if __name__=='__main__':
     FILE_PATH="Network_Data\phisingData.csv"
-    DATABASE="KRISHAI"
+    DATABASE="ANSHUAI"
     Collection="NetworkData"
     networkobj=NetworkDataExtract()
     records=networkobj.csv_to_json_convertor(file_path=FILE_PATH)
     print(records)
     no_of_records=networkobj.insert_data_mongodb(records,DATABASE,Collection)
     print(no_of_records)
+
+
+
+
+
+
+
+
         
 
 
--- test_mongodb.py
+++ test_mongodb.py
@@ -1,7 +1,7 @@
 
-from pymongo.mongo_client import MongoClient
+from pymongo import MongoClient
 
-uri = "mongodb+srv://krishaitechnologies:<@password>@cluster0.wnqb6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+uri = "mongodb+srv://p20anshu05_db_user:<db_password>@cluster0.av5wxkj.mongodb.net/?appName=Cluster0"
 
 # Create a new client and connect to the server
 client = MongoClient(uri)
